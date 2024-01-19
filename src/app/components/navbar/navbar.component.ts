f3b9da23d220c0e6aeeab3b215186a5ba9493ee8
Kelven Wyllmes <kelvenwyllames@gmail.com>
2024-01-19 19:04:53 -0300

    Adicionando lógica ao componente de navegação. Implementando lógica para abrir/fechar o menu dropdown ao clicar no botão de toggle. Adiciona media queries para tornar o  menu responsivo em dispositivos móveis.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.sass'
+  styleUrls: ['./navbar.component.sass'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const toggleBtn: HTMLElement | null = document.querySelector('.toggle_btn');
+    const toggleBtnIcon: HTMLElement | null =
+      document.querySelector('.toggle_btn i');
+    const dropDownMenu: HTMLElement | null =
+      document.querySelector('.dropdown_menu');
 
+    if (toggleBtn && toggleBtnIcon && dropDownMenu) {
+      toggleBtn.onclick = function () {
+        dropDownMenu.classList.toggle('open');
+        const isOpen: boolean = dropDownMenu.classList.contains('open');
+
+        toggleBtnIcon.className = isOpen
+          ? 'fa-solid fa-xmark'
+          : 'fa-solid fa-bars';
+      };
+    }
+  }
 }
